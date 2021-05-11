46fda35fc60948704927286883ea495cfd99bf4b
RoshniCh <roshni.chandrasekar@softwire.com>
2021-05-11 13:26:39 +0100

    va;idations

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using UserManagement.Repositories;
 using UserManagement.Models.Request;
 using UserManagement.Models.Database;
+using System.Text.RegularExpressions;
 using System;
 
 namespace UserManagement.Controllers {
@@ -21,7 +22,22 @@ namespace UserManagement.Controllers {
             {
                 return BadRequest(ModelState);
             }
+            
+            var isValid = CheckEmail(newUser.email);
+            if (!isValid){
+                return StatusCode(403, "Invalid email");
+            }
+            var validGivenName = CheckName(newUser.givenName);
+            if (!validGivenName){
+                return StatusCode(403, "Invalid given name");
+            }
+            var validFamilyName = CheckName(newUser.familyName);
+            if (!validFamilyName){
+                return StatusCode(403, "Invalid family name");
+            }
+
             var userCreated = _users.Create(newUser);
+            
             if (userCreated == null)
             {
                 return StatusCode(401);
@@ -44,11 +60,26 @@ namespace UserManagement.Controllers {
             {
                 return BadRequest(ModelState);
             }
+
+            var isValid = CheckEmail(updateUser.email);
+            if (!isValid){
+                return StatusCode(403, "Invalid email");
+            }
+            var validGivenName = CheckName(updateUser.givenName);
+            if (!validGivenName){
+                return StatusCode(403, "Invalid given name");
+            }
+            var validFamilyName = CheckName(updateUser.familyName);
+            if (!validFamilyName){
+                return StatusCode(403, "Invalid family name");
+            }
+            
             var userInfo = _users.Read(updateUser.id);
             if (userInfo == null)
             {
-                return StatusCode(204);
+                return StatusCode(403, "User does not exist");
             }
+            
             userInfo.email = updateUser.email;
             userInfo.givenName = updateUser.givenName;
             userInfo.familyName = updateUser.familyName;
@@ -74,7 +105,7 @@ namespace UserManagement.Controllers {
             var userInfo = _users.Read(id);
             if (userInfo == null)
             {
-                return StatusCode(204);
+                return StatusCode(403, "User does not exist");
             }
             var deletedUser = _users.Delete(userInfo);
             if (deletedUser == null)
@@ -85,6 +116,26 @@ namespace UserManagement.Controllers {
             {
                 return StatusCode(200);
             }
+        }
+
+        public bool CheckEmail(string email) {
+            Regex regex = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+            RegexOptions.CultureInvariant | RegexOptions.Singleline);
+            bool isValidEmail = regex.IsMatch(email);
+            return isValidEmail;
+        }
+
+        public bool CheckName(string name) {
+            bool validName;
+            if (name == "")
+            {
+                validName = false;
+            }
+            else
+            {
+                validName = true;
+            }
+            return validName;
         }
     }
 }
